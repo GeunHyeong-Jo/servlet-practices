ed61bc5a50ce221598ee188c0305f37cc528759b
jgh03 <jgh03@DESKTOP-9JBI22T.mshome.net>
2021-03-10 20:32:11 +0900

    sql_update  sql_schema update

--- servlet-practices/emaillist01/sql/query연습.sql
+++ servlet-practices/emaillist01/sql/query연습.sql
@@ -4,3 +4,28 @@ insert into emaillist values(null,'조','근형','jgh0316@naver.com');
 
 -- list
 select no, first_name, last_name, email from emaillist order by no desc;
+
+
+---------------------------------------------------------------------
+
+-- 이메일리스트
+DROP TABLE IF EXISTS `emaillist` RESTRICT;
+
+-- 이메일리스트
+CREATE TABLE `emaillist` (
+	`no`         INT UNSIGNED NOT NULL COMMENT '번호', -- 번호
+	`first_name` VARCHAR(20)  NOT NULL COMMENT '성', -- 성
+	`last_name`  VARCHAR(50)  NOT NULL COMMENT '이름', -- 이름
+	`email`      VARCHAR(200) NOT NULL COMMENT '이메일' -- 이메일
+)
+COMMENT '이메일리스트';
+
+-- 이메일리스트
+ALTER TABLE `emaillist`
+	ADD CONSTRAINT `PK_emaillist` -- 이메일리스트 기본키
+		PRIMARY KEY (
+			`no` -- 번호
+		);
+
+ALTER TABLE `emaillist`
+	MODIFY COLUMN `no` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '번호';
--- servlet-practices/guestbook01/sql/guestbook.sql
+++ servlet-practices/guestbook01/sql/guestbook.sql
@@ -8,3 +8,30 @@
  
  -- delete
 delete from guestbook where no = 1 and password='1234';
+
+
+
+
+------------------------------------------
+-- 방명록
+DROP TABLE IF EXISTS `guestbook` RESTRICT;
+
+-- 방명록
+CREATE TABLE `guestbook` (
+	`no`       INT UNSIGNED NOT NULL COMMENT '번호', -- 번호
+	`name`     VARCHAR(50)  NOT NULL COMMENT '이름', -- 이름
+	`password` VARCHAR(20)  NOT NULL COMMENT '비밀번호', -- 비밀번호
+	`contents` TEXT         NOT NULL COMMENT '내용', -- 내용
+	`reg_date` DATETIME     NOT NULL COMMENT '등록일' -- 등록일
+)
+COMMENT '방명록';
+
+-- 방명록
+ALTER TABLE `guestbook`
+	ADD CONSTRAINT `PK_guestbook` -- 방명록 기본키
+		PRIMARY KEY (
+			`no` -- 번호
+		);
+
+ALTER TABLE `guestbook`
+	MODIFY COLUMN `no` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '번호';
